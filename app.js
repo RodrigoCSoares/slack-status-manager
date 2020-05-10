90eaf5a9b13c5247ca422e2659c67f72f0e903df
Rodrigo Soares <soares.c.rodrigo@gmail.com>
2020-05-09 22:21:26 -0300

    Added bolt framework

---
+++ app.js
@@ -0,0 +1,31 @@
+    const { App } = require("@slack/bolt");
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET
+});
+
+app.event("app_home_opened", async ({ event, context }) => {
+  try {
+    console.log("App home opened!")
+  } catch (error) {
+    console.error(error);
+    console.error(JSON.stringify(error))
+  }
+});
+
+app.command('/lunch', async ({ command, ack, say }) => {
+  try {
+    await ack();
+
+    await say(`${command.text}`);
+  } catch (error) {
+    console.error(JSON.stringify(error))
+  }
+});
+
+(async () => {
+  await app.start(process.env.PORT || 3000);
+
+  console.log("⚡️ Bolt app is running!");
+})();
