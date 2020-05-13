c495b65f8ffcb75e0916e53c18ec08f9ed4f3bc8
Rodrigo Soares <soares.c.rodrigo@gmail.com>
2020-05-12 21:42:30 -0300

    Added Gulp and TypeScript

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,11 @@
-const { App } = require("@slack/bolt");
+import { App } from "@slack/bolt";
 
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET
 });
 
-app.event("app_home_opened", async ({ event, context }) => {
+app.event("app_home_opened", async () => {
   try {
     console.log("App home opened!")
   } catch (error) {
